dd4e98ef7a351e2e36aa1729aa4c444ce29f693b
Marcin Miotk <kmicik12@gmail.com>
2021-06-26 15:36:25 +0200

    Zadanie 6

--- src/Car.java
+++ src/Car.java
@@ -1,3 +1,15 @@
-public class Car extends  Vehicle{
+import java.io.Console;
+
+public class Car extends  Vehicle implements  Actions{
     int liczbaDrzwi;
+
+    @Override
+    public void Jedz() {
+        System.out.println("Pojazd porusza się");
+    }
+
+    @Override
+    public void Tankuj() {
+        System.out.println("Pojazd zsotał zatankowany");
+    }
 }
